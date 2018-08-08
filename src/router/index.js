7fedb86728e16ecd2337451723bc483f5653753f
lixiaoxiao <12324>
2018-08-08 11:14:01 +0800

    shiyan

--- src/router/index.js
+++ src/router/index.js
@@ -2,34 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import leftNav from '@/components/leftNav'     //头部
 // require.ensure(dependencies: String[], callback: function(require), chunkName: String)
-const login = r => require.ensure([], () => r(require('@/view/login')), 'login');
-const index = r => require.ensure([], () => r(require('@/view/index')), 'index');
-const movieDetail = r => require.ensure([], () => r(require('@/view/children/movieDetail')), 'movieDetail');
-const shap = r => require.ensure([], () => r(require('@/components/shap')), 'shap');
-// import index from '@/view/index'
-// import movie from '@/view/movie'
-// import movieDetail from '@/view/children/movieDetail'
-// import shap from '@/components/shap'
+const login = r => require.ensure([], () => r(require('@/view/login')), 'login')
+const index = r => require.ensure([], () => r(require('@/view/index')), 'index')
+
 Vue.use(Router)
-//如果页面是公用一个头部则可以将公共头尾写在app.vue中 否则可以头部为首页 其它页面为二级页面进行使用
+
 export default new Router({
-  mode:"history",
+  // mode:"history",
   routes: [
     {
-      path: '/leftNav',      //顶部导航
+      path: '/leftNav',      /*顶部导航*/
       name: 'leftNav',
       component: leftNav,
       redirect:'/index',
       children:[
         {
-          path: '/index',      //首页
+          path: '/index',
           name: 'index',
           component:index
-        },
-        {
-          path:'/movieDetail', //电影详情页
-          name:'movieDetail',
-          component:movieDetail
         }
       ]
     },
