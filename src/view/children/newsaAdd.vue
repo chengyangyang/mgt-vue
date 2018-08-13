d4e5545b8e291e1410f02b664725e41e2f21e40b
lixiaoxiao <12324>
2018-08-13 09:59:14 +0800

    voce

---
+++ src/view/children/newsaAdd.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="newsAdd">
+
+  </div>
+</template>
+
+<script>
+  // import pageNation from '@/components/pageNation'   // 分页组件
+  export default {
+    name:"news2",
+    data(){
+      return{
+
+      }
+    },
+    components:{
+        // pageNation
+    },
+    mounted(){
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style lang="less">
+  #newsAdd{
+
+  }
+</style>
