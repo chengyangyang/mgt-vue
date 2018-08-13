0f46a341049dcecd00b6a6cc3fcee9410aa2cc83
zz137516574 <137516574@qq.com>
2018-08-13 16:40:13 +0800

    x

--- src/mock/new1.js
+++ src/mock/new1.js
@@ -1,1 +1,37 @@
-console.log(8888888)
+// 使用 Mock
+var Mock = require('mockjs');
+
+// 获取 mock.Random 对象
+const Random = Mock.Random;
+
+let articles = [];
+for (let i = 0; i < 15; i++) {
+  let newsObject = {
+    title: Random.csentence(5, 30), // Random.csentence( min, max )
+    unit: Random.cname(), // Random.cname() 随机生成一个常见的中文姓名
+    types: Random.tld(), // Random.cname() 随机生成一个类型
+    stated: Random.cfirst(), // Random.cname() 随机生成一个状态
+    published: Random.datetime() // Random.date()指示生成的日期字符串的格式,默认为yyyy-MM-dd；Random.time() 返回一个随机的时间字符串
+  }
+  articles.push(newsObject)
+}
+var totalPageNum = articles.length;
+
+// mock一组数据
+const productData = function (opt) {
+  var page = JSON.parse(opt.body).page;
+  var pageNumber = JSON.parse(opt.body).pageNumber;
+  var newArticles = articles.slice((page - 1) * pageNumber, page * pageNumber);
+  if (JSON.parse(opt.body).title) {
+    newArticles = newArticles.filter(item => item.title === JSON.parse(opt.body).title);
+    totalPageNum = newArticles.length
+  }
+  return {
+    data: newArticles,
+    totalPage: totalPageNum
+  }
+}
+
+// mock一组数据
+
+Mock.mock('/news1', /post|get/i, productData) // 当post或get请求到/news路由时Mock会拦截请求并返回上面的数据
--- src/view/children/news1.vue
+++ src/view/children/news1.vue
@@ -1,30 +1,158 @@
 <template>
   <div id="news1">
-    <p style="background: #fafafa;
-    line-height: 35px;
-    padding: 6px 10px;
-    border-bottom: 2px solid #edf0f5;">
-      news1
-    </p>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/#/index' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>新闻资讯</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="mgt-content-box">
+      <div class="ibox">
+        <div class="title">
+          <el-button type="primary" icon="el-icon-plus">添加新闻资讯</el-button>
+        </div>
+        <div class="detail">
+          <el-form :inline="true" :model="searchData" class="demo-form-inline">
+            <el-form-item label="标题">
+              <el-input v-model="searchData.title" placeholder="标题"></el-input>
+            </el-form-item>
+            <el-form-item label="发布单位">
+              <el-input v-model="searchData.unit" placeholder="发布单位"></el-input>
+            </el-form-item>
+            <el-form-item label="文章类型">
+              <el-select v-model="searchData.types" placeholder="文章类型">
+                <el-option v-for="item in newsOptions" :key="item.newsVal" :label="item.label" :value="item.newsVal"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="发布时间">
+              <el-date-picker v-model="searchData.published" type="datetimerange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期"></el-date-picker>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubForm1">查询</el-button>
+            </el-form-item>
+          </el-form>
+
+          <el-table :data="tableData" stripe style="width: 100%">
+            <el-table-column type="index" label="序号" prop="idx" width="80"></el-table-column>
+            <el-table-column prop="title" label="标题"></el-table-column>
+            <el-table-column prop="unit" label="发布单位"></el-table-column>
+            <el-table-column prop="types" label="文章类型" width="180"></el-table-column>
+            <el-table-column prop="stated" label="发布/关闭" width="180"></el-table-column>
+            <el-table-column prop="published" label="发布时间" width="180"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-button size="mini" type="primary" @click="handleDetail(scope.$index, scope.row)">查看</el-button>
+                <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+
+          <div class="page-turn text-center">
+            <v-pageNation ref="myChid" :searchData="searchData"  :resourceUrl="resourceUrl" @askData="listData"></v-pageNation>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import pageNation from '../../components/pageNation.vue';
   export default {
     name:"news1",
     data(){
       return{
-
+        newsData: '',
+        searchData: {
+          title: '',
+          unit: '',
+          types: '',
+          published: ''
+        },
+        newsOptions: [
+          {
+            typeVal: '选项1',
+            label: '黄金糕'
+          },
+          {
+            typeVal: '选项2',
+            label: '双皮奶'
+          },
+          {
+            typeVal: '选项3',
+            label: '蚵仔煎'
+          }
+        ],
+        tableData: '', //列表总数据
+        resourceUrl:{url:"/news1"},
+//        published: [new Date(2018, 8, 10, 0, 0), new Date(2018, 10, 1, 0, 0)],
       }
     },
     components:{
+      'v-pageNation' : pageNation
     },
     mounted(){
     },
     methods:{
+      //表格操作事件
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDetail(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      },
+      //列表数据
+      listData(data){
+        this.tableData = data;
+      },
+     //搜索事件
+      onSubForm1() {
+        this.$refs.myChid.setNewsApi();
+      },
     }
   }
 </script>
 
 <style lang="less">
+  #news1{
+    zoom: 0.9;
+  }
+  .el-breadcrumb{
+    background: #fafafa;
+    line-height: 37px;
+    padding: 10px 10px;
+    border-bottom: 2px solid #edf0f5;
+  }
+  .mgt-content-box{
+    padding: 10px 20px;
+    .ibox{
+      background: #fff;
+      padding: 0 20px;
+      margin: 10px;
+      border: 1px solid #dee5f1;
+      .title {
+        border-bottom: 1px solid #dee5f1;
+        font-size: 16px;
+        line-height: 80px;
+      }
+      .detail {
+        padding: 20px 0;
+        th.is-leaf{
+          background: #409EFF;
+          color: #ffffff;
+        }
+        .page-turn{
+          margin-top: 35px;
+        }
+        .text-align{
+          text-align: center;
+        }
+      }
+    }
+  }
+  .el-table td, .el-table th.is-leaf{
+    text-align: center;
+  }
 </style>
